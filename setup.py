939e628ecb307a8a2a725e8eda7ed009e538601b
Victor <viktour19@gmail.com>
2024-02-21 18:11:23 -0500

    refactor setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,45 @@
-from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools_cuda_cpp import CUDAExtension, BuildExtension
 import os
 import numpy
+from pathlib import Path
 
 cuda_home = os.environ.get('CUDA_HOME', '/usr/local/cuda-12.2')
 cuda_include_dir = os.path.join(cuda_home, 'include')
 cuda_lib_dir = os.path.join(cuda_home, 'lib64')
 gpu_arch = os.environ.get('GPU_ARCH', 'sm_86')
 
+def append_nvcc_threads(nvcc_extra_args):
+    return nvcc_extra_args + ['--threads', '4']
+
 cuda_ext_path = Path('src/culingam')
 cuda_ext = CUDAExtension(
     name='lingam_cuda',
     include_dirs=[cuda_ext_path / 'include', cuda_include_dir, numpy.get_include()],
     sources=[
-        cuda_ext_path / 'basic.cu',
-        cuda_ext_path / 'basic_wrapper.cpp',
+        str(cuda_ext_path / 'basic.cu'),
+        str(cuda_ext_path / 'basic_wrapper.cpp'),
     ],
     libraries=['cudart', 'cudadevrt', 'nvToolsExt'],
-    dlink=True,
-    dlink_libraries=['cudart', 'cudadevrt', 'nvToolsExt'],
     extra_compile_args={
-        'cxx': ['-g','-std=c++17'],
-        'nvcc': ['-std=c++17', f'-arch={gpu_arch}'],
+        'cxx': ['-g', '-std=c++17'],
+        'nvcc': append_nvcc_threads([
+            '-O3',
+            '-std=c++17',
+            f'-arch={gpu_arch}'
+        ])
     },
 )
 
 setup(
-    ext_modules=[cuda_ext],
-    cmdclass={'build_ext': BuildExtension},
+    name='culingam',
+    version='0.0.4',
+    author='Victor Akinwande',
     description='CULiNGAM accelerates LiNGAM analysis on GPUs.',
-    version='0.0.1',
-    author='Victor Akinwande'
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    ext_modules=[cuda_ext],
+    cmdclass={'build_ext': BuildExtension}
 )
--- src/culingam/__init__.py
+++ src/culingam/__init__.py
@@ -1,5 +1,5 @@
 """
 CULiNGAM accelerates LiNGAM analysis on GPUs.
 """
-__version__ = '0.0.1'
+__version__ = '0.0.4'
 __all__ = ['directlingam', 'varlingam']
